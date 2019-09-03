c01b0b46126edbc1920780eca14f99350f91af62
Dino Tudor <dinotudor@gmail.com>
2019-09-03 19:53:30 +0200

    WIP style container and form

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,9 +1,26 @@
 import React from 'react';
 
-import { Title } from './styles';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
+
+import { Container, Form, SubmitButton } from './styles';
 
 function Main() {
-  return <Title>Hello world</Title>;
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Respositorios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar repositório" />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 export default Main;
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,7 +1,29 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  font-size: 24px;
-  color: #fff
-  font-family: Arial, Helvetica, sans-serif;
+export const Container = styled.div`
+  max-width: 700px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 80px auto;
+
+  h1 {
+    font-size: 20px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    svg {
+      margin-rigth: 10px;
+    }
+  }
 `;
+
+export const Form = styled.div`
+  margin-top: 30px;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const SubmitButton = styled.div``;
